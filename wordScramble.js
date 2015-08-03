34d2f531f80da0112faf0713b514c10d0e391a42
Gregg Hanano <gregg.hanano@gmail.com>
2015-08-02 21:43:46 -0700

    changed file name from app to wordScramble

--- wordScramble.js
+++ wordScramble.js
@@ -13,8 +13,7 @@ myApp.controller('wordScramble',function ($scope, $http){
 			var value = String.fromCharCode(e.keyCode).toLowerCase();
 			console.log(value);
 			$scope.checkLetter(value, e.keyCode);
-
-	}]);
+	});
 
 	$scope.randomWord = function(){
 		var path = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=adverb&excludePartOfSpeech=verb&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=8&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
